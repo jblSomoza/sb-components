b76c3aefd22f967104b58295c61dbbf2513f9ce0
Jhony Bryan López Somoza <Jhony.BLS@anacafe.org>
2022-08-23 10:02:14 -0600

    feat: agregamos semantic-versionling

---
+++ dist/stories/components/MyLabel.stories.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="react" />
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+declare const _default: ComponentMeta<({ AllCaps, color, label, size, fontColor, backgroundColor }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export default _default;
+export declare const Basic: ComponentStory<({ AllCaps, color, label, size, fontColor, backgroundColor }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export declare const AllCaps: ComponentStory<({ AllCaps, color, label, size, fontColor, backgroundColor }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export declare const Secondary: ComponentStory<({ AllCaps, color, label, size, fontColor, backgroundColor }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export declare const Tertiary: ComponentStory<({ AllCaps, color, label, size, fontColor, backgroundColor }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export declare const CustomFontColor: ComponentStory<({ AllCaps, color, label, size, fontColor, backgroundColor }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
+export declare const CustomBackgroundColor: ComponentStory<({ AllCaps, color, label, size, fontColor, backgroundColor }: import("../../components/MyLabel").MyLabelProps) => JSX.Element>;
